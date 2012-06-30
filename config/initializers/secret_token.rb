7200945631ee1e8a77ecd5d960722e1c0ad65014
jackknife <jackknife@jackknife-Lenovo-S10-3c.(none)>
2012-06-30 18:15:33 +0530

    nightqueen first push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nightqueen::Application.config.secret_token = 'cbc5297ca73bbe1973e98fb531d2aeebfef0acc3005f967bc74cfabb803a5feda1e873f21f55e92fd581ab6756cdcb2840a912b3c146f4895b642e0d7b0b6132'
